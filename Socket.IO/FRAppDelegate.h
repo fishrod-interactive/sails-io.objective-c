f68e9bf9e3524457892d1bc4bbc41b293a0b39f0
Gavin Williams <gavin.williams@fishrod.co.uk>
2014-03-04 21:31:03 +0000

    Initial Commit

---
+++ Socket.IO/FRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FRAppDelegate.h
+//  Socket.IO
+//
+//  Created by Gavin Williams on 04/03/2014.
+//  Copyright (c) 2014 Fishrod Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
